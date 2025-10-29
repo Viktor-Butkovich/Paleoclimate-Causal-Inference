7e55bcdd8848014e6541921a2810d9c96273c609
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-10-28 19:33:01 -0500

    Added modern temperature records

--- ETL.py
+++ ETL.py
@@ -6,6 +6,7 @@ import os
 import pickle as pkl
 import polars as pl
 import netCDF4 as nc
+import numpy as np
 from modules import util
 import warnings
 
@@ -183,5 +184,141 @@ upper_bound = q3 + 1.5 * iqr
 temperature_df = temperature_df.filter(
     (temperature_df["degC"] >= lower_bound) & (temperature_df["degC"] <= upper_bound)
 )
+
+# %%
+# Add modern temperature anomalies
+if not os.path.exists("Data/precomputed_modern_temperature.parquet"):
+    array_data = modern_temperature_grid.variables["temperature"][:]
+    # Get the dimensions
+    months, latitudes, longitudes = array_data.shape
+
+    # Create arrays for month, latitude, and longitude
+    total_elements = months * latitudes * longitudes
+    indices = np.arange(total_elements)
+
+    # Calculate the original indices
+    month_array = indices // (latitudes * longitudes)
+    latitude_array = ((indices % (latitudes * longitudes)) // longitudes) - 89
+    longitude_array = (indices % longitudes) - 179
+
+    # Flatten the 3-dimensional array into a 1-dimensional array
+    flattened_array = array_data.flatten()
+
+    flattened_array: np.ma.masked_array = flattened_array
+
+    filled_array = flattened_array.filled(np.nan)
+
+    # Replace NaN values with the item at the index 1555200 indexes later
+    nan_indices = np.where(np.isnan(filled_array))[0]
+    for idx in reversed(nan_indices):
+        filled_array[idx] = filled_array[idx + 1555200]
+
+    # Create a Polars DataFrame
+    modern_temperature_df = pl.DataFrame(
+        {
+            "month": month_array,
+            "geo_meanLat": latitude_array,
+            "geo_meanLon": longitude_array,
+            "anomaly": filled_array,
+        }
+    )
+    # Remove odd latitudes and longitudes
+    modern_temperature_df = modern_temperature_df.filter(
+        (modern_temperature_df["geo_meanLat"] % 2 == 0)
+        & (modern_temperature_df["geo_meanLon"] % 2 == 0)
+    )
+
+    # Group by latitude and longitude, and calculate the average anomaly for each group of 12 months
+    modern_temperature_df = (
+        modern_temperature_df.with_columns((1850 + pl.col("month") // 12).alias("year"))
+        .group_by(["geo_meanLat", "geo_meanLon", "year"])
+        .agg(pl.col("anomaly").mean().alias("anomaly"))
+    )
+
+    modern_temperature_grid.variables["climatology"].shape
+    # Calculate the average annual temperature for each latitude and longitude combination
+    climatology_data = modern_temperature_grid.variables["climatology"][:]
+    # Get the dimensions
+    months, latitudes, longitudes = climatology_data.shape
+
+    # Calculate the average annual temperature
+    average_climate = climatology_data.mean(axis=0)
+    # Create arrays for latitude and longitude
+    latitude_array = np.arange(latitudes) - 89
+    longitude_array = np.arange(longitudes) - 179
+
+    # Create a Polars DataFrame
+    climatology_df = pl.DataFrame(
+        {
+            "geo_meanLat": np.repeat(latitude_array, longitudes),
+            "geo_meanLon": np.tile(longitude_array, latitudes),
+            "climate": average_climate.flatten(),
+        }
+    )
+    # Remove odd latitudes and longitudes
+    climatology_df = climatology_df.filter(
+        (climatology_df["geo_meanLat"] % 2 == 0)
+        & (climatology_df["geo_meanLon"] % 2 == 0)
+    )
+
+    # Merge climatology_df and grouped_df on latitude and longitude
+    modern_temperature_df = climatology_df.join(
+        modern_temperature_df, on=["geo_meanLat", "geo_meanLon"], how="inner"
+    )
+    modern_temperature_df = modern_temperature_df.with_columns(
+        (pl.col("climate") + pl.col("anomaly")).alias("degC")
+    ).drop("climate")
+
+    # Assign a unique sample_id to each unique pair of geo_meanLat and geo_meanLon
+    unique_locations = modern_temperature_df.select(
+        ["geo_meanLat", "geo_meanLon"]
+    ).unique()
+    unique_locations = unique_locations.with_row_index(name="sample_id")
+    unique_locations = unique_locations.with_columns(
+        (pl.col("sample_id") + num_samples + 1).alias("sample_id")
+    )  # Add num_samples to each sample_id to avoid duplicates
+
+    # Join the unique locations with the merged_df to assign sample_id
+    modern_temperature_df = modern_temperature_df.join(
+        unique_locations, on=["geo_meanLat", "geo_meanLon"], how="left"
+    )
+
+    modern_temperature_df = modern_temperature_df.with_columns(
+        pl.col("sample_id").cast(pl.Int64),
+        pl.col("year").cast(pl.Int64),
+        pl.col("degC").cast(pl.Float64),
+        pl.col("anomaly").cast(pl.Float64),
+        pl.col("geo_meanLat").cast(pl.Int64),
+        pl.col("geo_meanLon").cast(pl.Int64),
+    )
+    modern_temperature_df.write_parquet("Data/precomputed_modern_temperature.parquet")
+else:
+    modern_temperature_df = pl.read_parquet(
+        "Data/precomputed_modern_temperature.parquet"
+    )
+temperature_df = pl.concat(
+    [
+        temperature_df,
+        modern_temperature_df.select(
+            [
+                "sample_id",
+                "year",
+                "degC",
+                "anomaly",
+                "geo_meanLat",
+                "geo_meanLon",
+            ]
+        ),
+    ]
+).with_row_index(name="temperature_id")
+
+# Assign measurements to year bins
+#   Since we are combining measurements from many sources and times, we need to bin them to avoid missing values
+temperature_df = temperature_df.with_columns(
+    pl.col("year")
+    .map_elements(util.get_year_bin, return_dtype=pl.Int64)
+    .alias("year_bin")
+).with_columns(pl.col("year_bin").alias("time_id"))
+valid_year_bins = list(temperature_df["year_bin"].unique())
 
 # %%
