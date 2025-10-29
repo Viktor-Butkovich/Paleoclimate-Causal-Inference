3be7cce46456c6506a079b957fd23fc1134ba7d9
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-10-28 19:24:27 -0500

    Added Temp12k ETL

--- ETL.py
+++ ETL.py
@@ -1,16 +1,23 @@
 # %%
+# Imports
+
 import requests
 import os
+import pickle as pkl
+import polars as pl
+import netCDF4 as nc
+from modules import util
+import warnings
 
 # %%
-
-url = "https://www.ncei.noaa.gov/pub/data/paleo/reconstructions/climate12k/temperature/version1.0.0/Temp12k_v1_0_0.pkl"
-# Sourced from https://www.ncei.noaa.gov/access/paleo-search/study/27330
-
-local_filename = "Data/Temp12k_v1_0_0.pkl"
+# Extract web data files
+warnings.filterwarnings("ignore", category=UserWarning)
 
 
 def download_file(url, filename):
+    if os.path.exists(filename):
+        print(f"{filename} already exists. Skipping download.")
+        return
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     print(f"Downloading {filename} from {url}...")
     response = requests.get(url, stream=True)
@@ -21,6 +28,160 @@ def download_file(url, filename):
     print(f"Saved {filename}")
 
 
-download_file(url, local_filename)
+data_sources = {
+    "temp12k": {
+        "url": "https://www.ncei.noaa.gov/pub/data/paleo/reconstructions/climate12k/temperature/version1.0.0/Temp12k_v1_0_0.pkl",
+        "path": "Data/Temp12k_v1_0_0.pkl",
+        "source": "https://www.ncei.noaa.gov/access/paleo-search/study/27330",
+    },
+    "berkeley_earth": {
+        "url": "https://berkeley-earth-temperature.s3.us-west-1.amazonaws.com/Global/Gridded/Land_and_Ocean_LatLong1.nc",
+        "path": "Data/Berkeley_Earth_Land_and_Ocean_LatLong1.nc",
+        "source": "http://berkeleyearth.org/data/",
+    },
+}
+for data_source in data_sources.values():
+    download_file(data_source["url"], data_source["path"])
+
+# %%
+# Load data files locally
+temp12k = pkl.load(open(data_sources["temp12k"]["path"], "rb"))
+modern_temperature_grid = nc.Dataset(data_sources["berkeley_earth"]["path"], mode="r")
+
+# %%
+# Transform data into a tabular format
+month_indexes = {
+    "january": 0,
+    "february": 1,
+    "march": 2,
+    "april": 3,
+    "may": 4,
+    "june": 5,
+    "july": 6,
+    "august": 7,
+    "september": 8,
+    "october": 9,
+    "november": 10,
+    "december": 11,
+    "winter": 0,
+    "spring": 3,
+    "summer": 6,
+    "fall": 9,
+    "cold season": 0,
+    "coldest month": 0,
+    "coldest": 0,
+    "mean summer": 6,
+    "1; summer": 6,
+    "1 (summer)": 6,
+    "warmest month": 6,
+    "warmest": 6,
+}
+temperature_data = []
+units = set()
+num_samples = 0
+ages = []
+outliers = []
+rewritten_samples = []
+
+for sample in temp12k["TS"]:
+    units.add(sample.get("paleoData_units"))
+    if util.include_sample(sample):
+        if (
+            sample.get("paleoData_interpretation")
+            and sample["paleoData_interpretation"][0].get("seasonality", "").lower()
+            in month_indexes
+        ):
+            month_idx = month_indexes[
+                sample["paleoData_interpretation"][0]["seasonality"].lower()
+            ]
+        else:
+            # Some samples labeled with month as habitatSeason, some labeled in interpretation
+            month_idx = month_indexes.get(
+                sample.get("paleoData_habitatSeason", None), None
+            )
+        lat, lon = round(sample.get("geo_meanLat")), round(sample.get("geo_meanLon"))
+
+        num_samples += 1
+        if month_idx == None:
+            climate = util.get_climate(modern_temperature_grid, lat, lon)
+        else:
+            climate = util.get_climate_month(
+                modern_temperature_grid, lat, lon, month_idx
+            )
+
+        anomalies = []
+        most_recent_age = sample["age"][0]
+        most_recent_temperature = sample["paleoData_values"][0]
+        temperature_offset = 0
+        if most_recent_age != "nan" and most_recent_temperature != "nan":
+            if most_recent_age < -50:
+                # If most recent age is in the future, adjust to be present
+                #   Most samples use age as years before 1950, but some seem to use a different convention
+                shift_forward = -50 - most_recent_age
+                sample["age"] = [
+                    age + shift_forward for age in sample["age"] if age != "nan"
+                ]
+                most_recent_age = sample["age"][0]
+            most_recent_year = 1950 - int(most_recent_age)
+            if most_recent_year > 0:
+                if not "T anomalies" in sample.get("paleoData_description", ""):
+                    if most_recent_year >= 1850:
+                        if month_idx == None:
+                            most_recent_anomaly = util.get_anomaly_when(
+                                modern_temperature_grid, lat, lon, most_recent_year
+                            )
+                        else:
+                            most_recent_anomaly = util.get_anomaly_when_month(
+                                modern_temperature_grid,
+                                lat,
+                                lon,
+                                most_recent_year,
+                                month_idx,
+                            )
+                    else:
+                        most_recent_anomaly = util.get_anomaly_when(
+                            modern_temperature_grid, lat, lon, 1850
+                        )
+                    # Add most recent temperature to each data point, such that the most recent temperature is assumed to be the modern average
+                    #   Thus, even if the most recent temperature is different from the modern average (local variation, error), we still get an accurate anomaly vs age
+        for age, temperature in zip(sample["age"], sample["paleoData_values"]):
+            # Convert age (years BP) to a date (assuming current year is 1950 for BP conversion)
+            if age != "nan" and temperature != "nan":
+                diff = float(temperature) - most_recent_temperature
+                anomaly = (
+                    diff + most_recent_anomaly
+                )  # Total anomaly is difference from most recent + anomaly of most recent
+                temperature = climate + anomaly
+                temperature_data.append(
+                    {
+                        "sample_id": num_samples,
+                        "year": round(
+                            most_recent_year - (float(age) - most_recent_age)
+                        ),
+                        "degC": temperature,
+                        "anomaly": anomaly,
+                        "geo_meanLat": lat,
+                        "geo_meanLon": lon,
+                    }
+                )
+                ages.append(age)
+                anomalies.append(temperature_data[-1]["anomaly"])
+        if abs(temperature_data[-1]["anomaly"]) > 20:
+            sample["anomaly"] = anomalies
+            rewritten_samples.append(sample)
+
+# %%
+# Convert tabular temperature data to a Polars DataFrame
+temperature_df = pl.DataFrame(temperature_data)
+# Remove temperature outliers that are more than 1.5 IQR away
+q1 = temperature_df["degC"].quantile(0.25)
+q3 = temperature_df["degC"].quantile(0.75)
+iqr = q3 - q1
+lower_bound = q1 - 1.5 * iqr
+upper_bound = q3 + 1.5 * iqr
+
+temperature_df = temperature_df.filter(
+    (temperature_df["degC"] >= lower_bound) & (temperature_df["degC"] <= upper_bound)
+)
 
 # %%
---
+++ modules/util.py
@@ -0,0 +1,165 @@
+from typing import List
+import polars as pl
+import netCDF4 as nc
+import numpy as np
+
+
+def get_year_bin(year: int) -> int:
+    if year < -800000:  # Nearest 50,000
+        return round(year / 50000) * 50000
+    elif year < -20000:  # Nearest 2000
+        return round(year / 2000) * 2000
+    elif year < 0:  # Nearest 250
+        return round(year / 250) * 250
+    elif year < 1850:  # Nearest 50
+        return round(year / 50) * 50
+    elif year < 2025:  # Nearest 1
+        return round(year)
+    else:  # Next 1000
+        return round((year + 1000) / 1000) * 1000
+
+
+def include_sample(sample) -> bool:
+    if sample.get("paleoData_units") != "degC":
+        return False
+    elif sample.get("paleoData_useInGlobalTemperatureAnalysis", "TRUE") == "FALSE":
+        return False
+    elif "DELETE" in sample.get("paleoData_QCnotes", ""):
+        return False
+    elif sample.get("age", None) == None:
+        return False
+    elif sample.get("paleoData_values")[0] == "nan" or sample.get("age")[0] == "nan":
+        return False
+    else:
+        return True
+
+
+def year_bins_transform(df: pl.DataFrame, valid_year_bins: List[int]) -> pl.DataFrame:
+    df = (
+        df.with_columns(
+            pl.col("year")
+            .map_elements(get_year_bin, return_dtype=pl.Int64)
+            .alias("year_bin")
+        )
+        .group_by("year_bin")
+        .agg([pl.col(col).mean().alias(col) for col in df.columns if col != "year_bin"])
+        .drop("year")
+    )
+    missing_year_bins = set(valid_year_bins) - set(df["year_bin"].unique())
+    missing_entries = pl.DataFrame(
+        {
+            "year_bin": list(missing_year_bins),
+            **{
+                col: [None] * len(missing_year_bins)
+                for col in df.columns
+                if col != "year_bin"
+            },
+        }
+    )
+
+    df = pl.concat([df, missing_entries]).sort("year_bin")
+
+    for col in df.columns:
+        if col != "year_bin":
+            df = df.with_columns(pl.col(col).interpolate().alias(col))
+
+    df = df.with_columns(
+        [
+            pl.col(col).fill_null(strategy="backward").fill_null(strategy="forward")
+            for col in df.columns
+        ]
+    )
+
+    for col in [
+        "co2_ppm",
+        "co2_radiative_forcing",
+        "anomaly",
+        "be_ppm",
+        "VADM",
+    ]:  # Remove future filled null values
+        if col in df.columns:
+            df = df.with_columns(
+                pl.when(pl.col("year_bin") > 2025)
+                .then(None)
+                .otherwise(pl.col(col))
+                .alias(col)
+            )
+
+    return df
+
+
+def get_climate_month(modern_temperature_grid: nc.Dataset, lat, lon, month_idx):
+    """
+    Gets the average temperature for the nearest latitude and longitude.
+
+    Parameters:
+    dataset (Dataset): The netCDF4 dataset.
+    lat (float): The latitude.
+    lon (float): The longitude.
+
+    Returns:
+    float: The average temperature.
+    """
+    # The temperature variable holds anomaly delta degC for each month since 1850, while the climatology variable holds the historical average degC for each month
+    return float(
+        modern_temperature_grid.variables["climatology"][
+            :, round(lat) + 89, round(lon) + 179
+        ][month_idx]
+    )
+
+
+def get_climate(modern_temperature_grid: nc.Dataset, lat, lon):
+    # The temperature variable holds anomaly delta degC for each month since 1850, while the climatology variable holds the historical average degC for each month
+    return float(
+        np.mean(
+            modern_temperature_grid.variables["climatology"][
+                :, round(lat) + 89, round(lon) + 179
+            ]
+        )
+    )
+
+
+def get_weighted_global_average_climate(modern_temperature_grid: nc.Dataset):
+    # Latitudes and longitudes are not proportional to surface area.
+    # To get a more accurate representation of the Earth's surface, we need to account for the cosine of the latitude.
+    # This is because the distance between lines of longitude decreases as you move towards the poles.
+    latitudes = np.arange(-90, 91, 1)
+    weights = np.cos(np.radians(latitudes))
+    weighted_sum = 0
+    total_weight = 0
+    for lat, weight in zip(latitudes, weights):
+        for lon in range(-180, 181, 1):
+            weighted_sum += get_climate(modern_temperature_grid, lat, lon) * weight
+            total_weight += weight
+    return weighted_sum / total_weight
+
+
+def get_anomaly_when_month(
+    modern_temperature_grid: nc.Dataset, lat, lon, year, month_idx
+):
+    temperature = modern_temperature_grid.variables["temperature"][
+        (year - 1850) * 12 + month_idx, lat, lon
+    ]
+    if (
+        type(temperature) == np.ma.core.MaskedConstant
+    ):  # Replace missing value with next year]
+        return get_anomaly_when_month(
+            modern_temperature_grid, lat, lon, year + 10, month_idx
+        )
+    else:
+        return float(temperature)
+
+
+def get_anomaly_when(
+    modern_temperature_grid: nc.Dataset, lat, lon, year, month_idx=None
+):
+    if month_idx == None:
+        month_time_series = [
+            get_anomaly_when_month(modern_temperature_grid, lat, lon, year, month_idx)
+            for month_idx in range(12)
+        ]
+        return float(np.mean(month_time_series))
+    else:
+        return get_anomaly_when_month(
+            modern_temperature_grid, lat, lon, year, month_idx
+        )
