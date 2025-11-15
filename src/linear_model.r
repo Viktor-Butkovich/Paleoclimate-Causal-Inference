f4e2012c8c6c753bcf3db8100da18e70d2e68df3
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-11-15 14:20:49 -0600

    Add basic linear model

---
+++ src/linear_model.r
@@ -0,0 +1,18 @@
+# Execute in VS Code with `Run Source With Echo` (default `ctrl + shift + enter`)
+
+if (!requireNamespace("here", quietly = TRUE)) {
+    install.packages("here")
+}
+if (!requireNamespace("tidyverse", quietly = TRUE)) {
+    install.packages("tidyverse")
+}
+
+library(here)
+library(tidyverse)
+
+df <- read_csv(here("Outputs", "anomaly_9k.csv"))
+df
+
+# Only include treatment and confounders that need to be adjusted for
+model <- lm(anomaly ~ co2_radiative_forcing + eccentricity + obliquity + perihelion + insolation + global_insolation + solar_modulation, data = df)
+summary(model)
