bff2c74b97070127b583455780458b315530d103
Jasmin Billingsley <jbhk8@umsystem.edu>
2025-12-09 17:22:37 -0600

    Create TMLE_PKG_9kold_Output.r
    *Outputs without volcanic_forcing included

---
+++ Outputs/TMLE_PKG_9kwovf_Output.r
@@ -0,0 +1,30 @@
+> TMLE_PKG
+ Marginal mean under treatment (EY1)
+   Parameter Estimate:  0.39652
+   Estimated Variance:  0.00016692
+              p-value:  <2e-16
+    95% Conf Interval:  (0.3712, 0.42185)
+
+ Marginal mean under comparator (EY0)
+   Parameter Estimate:  0.35343
+   Estimated Variance:  8.3731e-05
+              p-value:  <2e-16
+    95% Conf Interval:  (0.33549, 0.37136)
+
+ Additive Effect
+   Parameter Estimate:  0.043095
+   Estimated Variance:  7.9662e-05
+              p-value:  1.3764e-06
+    95% Conf Interval:  (0.025602, 0.060588)
+
+ Additive Effect among the Treated
+   Parameter Estimate:  0.18279
+   Estimated Variance:  0.00016854
+              p-value:  <2e-16
+    95% Conf Interval:  (0.15735, 0.20824)
+
+ Additive Effect among the Controls
+   Parameter Estimate:  0.41324
+   Estimated Variance:  0.00046911
+              p-value:  <2e-16
+    95% Conf Interval:  (0.37079, 0.45569)
