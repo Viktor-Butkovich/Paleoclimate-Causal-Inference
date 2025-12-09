eea05360aabcd5c0b12b6e45fe7108e91e3ff33e
Jasmin Billingsley <jbhk8@umsystem.edu>
2025-12-09 16:09:12 -0600

    Create TMLE_PKG_742k_Output.r

---
+++ Outputs/TMLE_PKG_742k_Output.r
@@ -0,0 +1,29 @@
+ Marginal mean under treatment (EY1)
+   Parameter Estimate:  0.46772
+   Estimated Variance:  0.00010078
+              p-value:  <2e-16
+    95% Conf Interval:  (0.44804, 0.4874)
+
+ Marginal mean under comparator (EY0)
+   Parameter Estimate:  0.45611
+   Estimated Variance:  0.00010347
+              p-value:  <2e-16
+    95% Conf Interval:  (0.43617, 0.47604)
+
+ Additive Effect
+   Parameter Estimate:  0.011611
+   Estimated Variance:  2.7586e-05
+              p-value:  0.027061
+    95% Conf Interval:  (0.0013166, 0.021905)
+
+ Additive Effect among the Treated
+   Parameter Estimate:  0.43571
+   Estimated Variance:  0.00044652
+              p-value:  <2e-16
+    95% Conf Interval:  (0.39429, 0.47712)
+
+ Additive Effect among the Controls
+   Parameter Estimate:  0.43047
+   Estimated Variance:  0.00060051
+              p-value:  <2e-16
+    95% Conf Interval:  (0.38244, 0.4785)
