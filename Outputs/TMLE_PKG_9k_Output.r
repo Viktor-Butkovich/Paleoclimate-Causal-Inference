7b0d5039e3a606d488e5c7f4051bfe34d41969d7
Jasmin Billingsley <jbhk8@umsystem.edu>
2025-12-09 16:08:25 -0600

    Create TMLE_PKG_9k_Output.r

---
+++ Outputs/TMLE_PKG_9k_Output.r
@@ -0,0 +1,29 @@
+ Marginal mean under treatment (EY1)
+   Parameter Estimate:  0.31936
+   Estimated Variance:  8.4668e-05
+              p-value:  <2e-16
+    95% Conf Interval:  (0.30133, 0.33739)
+
+ Marginal mean under comparator (EY0)
+   Parameter Estimate:  0.37148
+   Estimated Variance:  8.5352e-05
+              p-value:  <2e-16
+    95% Conf Interval:  (0.35338, 0.38959)
+
+ Additive Effect
+   Parameter Estimate:  -0.052125
+   Estimated Variance:  4.2837e-05
+              p-value:  1.664e-15
+    95% Conf Interval:  (-0.064953, -0.039297)
+
+ Additive Effect among the Treated
+   Parameter Estimate:  -0.43425
+   Estimated Variance:  0.0008839
+              p-value:  <2e-16
+    95% Conf Interval:  (-0.49252, -0.37598)
+
+ Additive Effect among the Controls
+   Parameter Estimate:  -0.14298
+   Estimated Variance:  0.00039625
+              p-value:  6.8387e-13
+    95% Conf Interval:  (-0.18199, -0.10396)
